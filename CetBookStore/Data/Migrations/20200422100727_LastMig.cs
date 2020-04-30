b31f7c447d4094c4fc61bf31a53a1595c1cb385f
EMRE.KAYMAZ <emre.kaymaz@enerjisauretim.com>
2020-04-30 11:29:40 +0300

    Hw3_1

---
+++ CetBookStore/Data/Migrations/20200422100727_LastMig.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CetBookStore.Data.Migrations
+{
+    public partial class LastMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Detail",
+                table: "Comments",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Books",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Books",
+                maxLength: 400,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Detail",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Books",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 400,
+                oldNullable: true);
+        }
+    }
+}
---
+++ CetBookStore/ViewModel/SearchViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewModel
+{
+    public class SearchViewModel
+    {
+         [Display(Name ="Arama Metni")]
+        public string SearchText { get; set; }
+        [Display(Name ="Açıklamalarda Ara")]
+        public bool SearchInDescription { get; set; }
+        [Display(Name ="Kategori Seçimi")]
+        public int? CategoryId { get; set; }
+        
+        [Display(Name ="Alt Limit")]
+        public int? MinPrice { get; set; }
+        [Display(Name ="Üst Limit")]
+        public int? MaxPrice { get; set; }
+
+        public List<Models.Book> Results { get; set; }
+    }
+}
